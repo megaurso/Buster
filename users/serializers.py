f62ed6d7f2eddb1a20660b2a85dba57d2855702a
megaurso <bernadogui@hotmail.com>
2023-04-10 15:31:46 -0300

    feat: task 1 done

---
+++ users/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=127, write_only=True)
+    email = serializers.EmailField()
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
+    is_superuser = serializers.BooleanField(read_only=True)
+
+    def validate_username(self,value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("username already taken.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("email already registered.")
+        return value
+    
+    
+    def create(self, validated_data: dict) -> User:
+        is_employee = validated_data.get("is_employee", False)
+        if is_employee:
+            validated_data["is_superuser"] = True
+        return User.objects.create_user(**validated_data)
---
+++ users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.views import APIView, Response, Request, status
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UsersView(APIView):
+    def get(self, req: Request) -> Response:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, req: Request) -> Response:
+        serializer = UserSerializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
